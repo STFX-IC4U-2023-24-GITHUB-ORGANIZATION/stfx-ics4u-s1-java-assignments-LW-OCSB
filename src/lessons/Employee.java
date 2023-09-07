79abf3b5be5d6dd2af4b95b41697dabaa2de762a
e28166 <e28166@10.20.1.185>
2023-09-07 10:34:53 -0400

    Lesson

---
+++ src/lessons/Employee.java
@@ -0,0 +1,25 @@
+package lessons;
+
+public class Employee {
+
+	// Data Member
+	private String name = "";
+	String birthDate = "";
+	String position = "";
+	
+	// get methods
+	
+	// get name
+	public String getName()  {
+		
+		return this.name;
+	}
+	
+	
+	// set methods
+	public void setName(String n)  {
+		
+		this.name = n;
+	}
+	
+}
---
+++ src/lessons/EmployeeTest.java
@@ -0,0 +1,32 @@
+package lessons;
+
+public class EmployeeTest {
+
+	public static void main(String[] args) {
+	
+		
+		// Variable declarations
+		
+		// short-cut to create common String object
+		String myString = "43r324r";
+		
+		// how to create an instance of an ojbect
+		String myString2 = new String("some text");
+
+		// create new Employee object
+		Employee employee1 = new Employee();
+		
+		employee1.setName("John Curley");
+		
+		System.out.println(employee1.getName());
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
