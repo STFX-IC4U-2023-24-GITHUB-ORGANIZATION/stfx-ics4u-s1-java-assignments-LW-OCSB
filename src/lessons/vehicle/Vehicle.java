0da50070dc959bf161f1fea32f3121ca19a9cb16
e28166 <e28166@10.20.1.185>
2023-09-11 10:05:01 -0400

    asdf

---
+++ src/lessons/vehicle/Vehicle.java
@@ -0,0 +1,86 @@
+package lessons.vehicle;
+
+public class Vehicle {
+
+	// data members
+	private String fuelType;
+	private String colour;
+	private int maxPass;
+	private boolean hasMotor;
+	
+	
+	// DESCRIPTION: default constructor 
+	// PARAMETER LIST: na
+	// RETURN TYPE: Vehicle 
+	public Vehicle() {
+		
+		fuelType = "";
+		colour = "";
+		maxPass = 0;
+		hasMotor = false;		
+	}
+	
+	// DESCRIPTION: parameterized constructor
+	// PARAMETER LIST: (String fuelType, String colour, int maxPass, boolean hasMotor)
+	// RETURN TYPE: Vehicle
+	public Vehicle(String ft, String c, int mp, boolean hm)  {
+		
+		this.fuelType = ft;
+		this.colour = c;
+		this.maxPass = mp;
+		this.hasMotor = hm;
+	}
+	
+	
+	// get methods
+	public String getFuelType()  {
+		
+		return this.fuelType;
+	}
+	
+	public String colour()  {
+
+		return this.colour;
+	}
+	
+	public int maxPass()  {
+		
+		return this.maxPass;
+	}
+	
+	public boolean hasMotor()  {
+		
+		return this.hasMotor;
+	}
+	
+	
+	// set methods
+	public void setFuelType(String ft)  {
+		
+		this.fuelType = ft;
+	}
+	
+	public void setColour(String c)  {
+		
+		this.colour = c;
+	}
+	
+	public void maxPass(int mp)  {
+		this.maxPass = mp;
+	}
+	
+	public void setHasMotor(boolean hm)  {
+		this.hasMotor = hm;
+	}
+	
+	@Override
+	public String toString()  {
+		
+		String str = "";
+		
+		str = super.toString() + this.fuelType + "." + this.colour + "."
+				+ this.maxPass + "." + this.hasMotor;
+				
+		return str;
+	}
+}
---
+++ src/lessons/vehicle/VehicleTest.java
@@ -0,0 +1,18 @@
+package lessons.vehicle;
+
+public class VehicleTest {
+
+	public static void main(String[] args) {
+
+
+		Vehicle myVehicle1 = new Vehicle();
+
+		Vehicle myVehicle2 = new Vehicle("Gas", "blue", 4, true);
+		
+		
+		System.out.println(myVehicle1);
+		System.out.println(myVehicle2);
+		
+	}
+
+}
