5e41078078ead828fff738681f56de9f9bd81cc5
e28166 <e28166@10.20.1.185>
2023-09-13 10:25:10 -0400

    s

---
+++ src/lessons/review/Casting.java
@@ -0,0 +1,47 @@
+package lessons.review;
+
+public class Casting {
+
+	public static void main(String[] args) {
+		
+		// int to String
+		String myString1 = "";
+		int myInteger1 = 13;		
+		myString1 = Integer.toString(myInteger1);
+		System.out.println(myInteger1);
+		
+		// String to int
+		String myString2 = "17";  // String must resolve to an integer
+							      // or Java runtime exception error
+		int myInteger2;
+		myInteger2 = Integer.parseInt(myString2);
+		System.out.println(myString2);
+		
+		// double to String
+		String myString3 = "";
+		double myDouble1 = 13.3;
+		myString3 = Double.toString(myDouble1);
+		System.out.println(myString3);
+		
+		// String to double
+		String myString4 = "6.8";
+		double myDouble2;
+		myDouble2 = Double.parseDouble(myString4);
+		System.out.println(myDouble2);
+		
+		// Integer to Double
+		int myInteger3 = 43;
+		double myDouble3 = (double)myInteger3;
+		System.out.println(myDouble3);
+		
+		// Double to Integer
+		double myDouble4 = 13.3;
+		int myInteger4 = (int)myDouble4;
+		System.out.println(myDouble4);
+		
+		
+		
+		
+	}
+
+}
