1cdd195dc676fb8655b6566e085836b05efb4b9d
e28166 <e28166@10.20.1.185>
2023-09-14 09:40:05 -0400

    s

---
+++ src/lessons/review/WhileLoops.java
@@ -0,0 +1,17 @@
+package lessons.review;
+
+public class WhileLoops {
+
+	public static void main(String[] args) {
+		
+		int counter = 10;
+		
+		while (counter != 0)  {
+			System.out.println("Counter: " + counter);
+			counter --;
+		}
+		
+
+	}
+
+}
