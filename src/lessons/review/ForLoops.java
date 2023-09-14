948afad3023faad81db40e330e9ddc83ffc0833a
e28166 <e28166@10.20.1.185>
2023-09-14 09:37:51 -0400

    s

---
+++ src/lessons/review/ForLoops.java
@@ -0,0 +1,26 @@
+package lessons.review;
+
+import java.util.Scanner;
+
+public class ForLoops {
+
+	public static void main(String[] args) {
+
+		// variables
+		Scanner sc = new Scanner(System.in);
+		int numHellos;
+		
+		System.out.println("Enter number of times to say hello: ");
+		numHellos = sc.nextInt();
+		
+		for (int i=0; i<numHellos; i++)  {
+			
+			System.out.println("Hello!");
+		}
+		
+		
+		
+
+	}
+
+}
