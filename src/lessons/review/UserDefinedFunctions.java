866850f81687c1a38714265645c4cc76c380874c
e28166 <e28166@10.20.1.185>
2023-09-28 09:56:58 -0400

    s

---
+++ src/lessons/review/UserDefinedFunctions.java
@@ -0,0 +1,19 @@
+package lessons.review;
+
+public class UserDefinedFunctions {
+
+	public static void main(String[] args) {
+		
+		System.out.println(sum(1, 2));
+		
+	}
+
+	// DESCRIPTION: Returns the sum of two ints.
+	// PARAMETERS:  sum(int a, int b)
+	// RETURN TYPE: int
+	public static int sum(int a, int b)  {
+		
+		return a + b;
+	}
+	
+}
