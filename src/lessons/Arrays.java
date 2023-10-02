eabf8579f3abd944e1f8b80dc1e2e6490e1e867e
e28166 <e28166@10.20.1.185>
2023-10-02 09:52:25 -0400

    s

---
+++ src/lessons/Arrays.java
@@ -0,0 +1,38 @@
+package lessons;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+
+		// Java arrays use single indexes, beginning with zero.
+		// They ELEMENTS of any SINGLE TYPE (primitives, objects).
+		// They are NOT dynamic (do not grow after creation).
+		
+		// Syntax:
+		// datatype[] arrayIdentifier = new datatype[arraySize];
+		
+		// examples:
+		String[] myStringArray = new String[3];
+		double[] myDoubleArray = new double[3];
+		int[] myIntArray = new int[3];
+		
+		myStringArray[0] = "foo";
+		myStringArray[1] = "bar";
+		myStringArray[2] = "fubar";
+		
+		myDoubleArray[0] = 13.3;
+		myDoubleArray[1] = 1.3;
+		myDoubleArray[2] = 3.3;
+		
+		myIntArray[0] = 12;
+		myIntArray[1] = 2;
+		myIntArray[2] = 5;
+		
+		for (int i=0; i<myStringArray.length; i++)  {
+		
+			System.out.println(myStringArray[i]);
+		}
+		
+	}
+
+}
