96c03c411d56b55485ddd30ac51c564c54484829
e28166 <e28166@10.20.1.185>
2023-11-13 10:45:08 -0500

    s

---
+++ src/lessons/ArrayListToArray.java
@@ -0,0 +1,35 @@
+package lessons;
+
+import java.util.ArrayList;
+
+public class ArrayListToArray {
+
+	public static void main(String[] args) {
+
+		// we need to know how to convert an ArrayList to an Array
+		// I'll tell you why later ...
+		
+		// ArrayList
+		ArrayList<String> myArrayList = new ArrayList();
+		myArrayList.add("dog");
+		myArrayList.add("cat");
+		myArrayList.add("mouse");
+		
+		// Array
+		String[] myArray = new String[myArrayList.size()];
+		
+		// copy all elements from ArrayList to Array:
+		myArrayList.toArray(myArray);
+		
+		// print contents of Array to check:
+		for (int i=0; i<myArray.length; i++)  {
+
+			System.out.print(myArray[i] + "\n");	
+		}
+		
+		
+		
+
+	}
+
+}
