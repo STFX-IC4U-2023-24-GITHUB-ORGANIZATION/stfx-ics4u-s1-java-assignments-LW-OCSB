063da86fa7061daff981e89c9a99104d1cdb735c
e28166 <e28166@10.20.1.185>
2023-11-30 10:43:58 -0500

    s

---
+++ src/lessons/BubbleSort.java
@@ -0,0 +1,47 @@
+package lessons;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+
+		int[] myArray = {6, 3, 1, 4, 0, 5, 2, 7};
+		
+		boolean swapped = true;
+
+		while (swapped == true)  {
+			
+			swapped = false;
+		
+			// code to keep passsing, comparing and swapping
+		
+			for (int i=0; i<myArray.length -2; i++)  {
+				
+				
+				if (myArray[i] > myArray[i+1])  {
+					
+					swap(myArray, i, i+1);
+					swapped = true;
+				}
+			}
+		}
+		
+		// print array
+		for (int i=0; i<myArray.length; i++)  {
+			
+			System.out.print(myArray[i] + " ");
+		}
+		
+		
+		
+	}
+
+	
+	public static void swap(int[] a, int x, int y)  {
+		
+		int temp = a[x];
+		
+		a[x] = a[y];
+		a[y] = temp;
+	}
+	
+}
